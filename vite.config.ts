e3fe762006f9d76b9c7389434dc7431d4cd66783
ozanArslan2424 <136094471+ozanArslan2424@users.noreply.github.com>
2026-01-31 16:55:19 +0300

    env vars and vite proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,31 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
-	build: {
-		assetsDir: "static",
-	},
-	server: {
-		proxy: {
-			"/api": {
-				target: "https://case.nodelabs.dev/api",
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, ""),
-				cookieDomainRewrite: "localhost", // This is the magic line
+export default defineConfig(({ mode }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+	return {
+		plugins: [react(), tailwindcss()],
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./src"),
 			},
 		},
-	},
+		build: {
+			assetsDir: "static",
+		},
+		server: {
+			proxy: {
+				"/api": {
+					target: process.env.VITE_API_URL,
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/api/, ""),
+					cookieDomainRewrite: process.env.VITE_DOMAIN,
+				},
+			},
+		},
+	};
 });
